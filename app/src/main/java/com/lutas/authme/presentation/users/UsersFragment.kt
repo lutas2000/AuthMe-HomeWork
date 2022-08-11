b4f092af233abdfaa2ca296bc05441cd10f310a0
Lutas Lin <wsrew2000@gmail.com>
2022-08-11 23:09:10 +0800

    feat: add item count

--- app/src/main/java/com/lutas/authme/presentation/users/UsersFragment.kt
+++ app/src/main/java/com/lutas/authme/presentation/users/UsersFragment.kt
@@ -1,9 +1,11 @@
 package com.lutas.authme.presentation.users
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -36,6 +38,9 @@ class UsersFragment : Fragment() {
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = usersAdapter
+        }
+        usersAdapter.addLoadStateListener {
+            binding.count = usersAdapter.itemCount
         }
     }
 
